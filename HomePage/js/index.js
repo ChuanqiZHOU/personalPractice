365a5576da98b2e5e26d6ffa73026fb6100e94d7
Bill ZHOU <chuanqizhou@gmail.com>
2021-10-24 10:05:29 +1300

    homepage nav finished

--- HomePage/js/index.js
+++ HomePage/js/index.js
@@ -1,7 +1,8 @@
 window.onload= function(){
 	let navBar = document.querySelector(".nav-bar");
 	let faBars = document.querySelector(".fa-bars-cursor");
-	let navLastSpanI = document.querySelector('nav span:last-child i');
+	let navLastSpanI = document.querySelector('.nav span:last-child i');
+	// let navLastSpan = document.querySelector('.nav span:last-child');
 	faBars.addEventListener('mouseenter', function(){
 		faBars.style.cursor = "pointer";
 	})
@@ -10,11 +11,14 @@ window.onload= function(){
 		if(flag == true){
 			navBar.style.display = "flex";
 			navBar.style.alignItems = "center";
+			// navLastSpan.innerHTML= '<i class = "fa fa-times fa-bars-cursor"></i>';
 			flag = false;
 			navLastSpanI.setAttribute('class', "fa fa-times fa-bars-cursor");
+			
 		}else {
 			navBar.style.display = "none";
-			navLastSpanI.setAttribute('class', "fa fa-bars fa-bars-cursor")
+			// navLastSpan.innerHTML= '<i class = "fa fa-bars fa-bars-cursor"></i>';
+			navLastSpanI.setAttribute('class', "fa  fa-bars fa-bars-cursor");
 			flag = true;
 		}	
 	});
