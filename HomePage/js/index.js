ebc1ce3fda8814216d3fff9a957c9bfc16abb4de
Bill ZHOU <chuanqizhou@gmail.com>
2021-11-02 14:09:35 +1300

    typed package test and practice

--- HomePage/js/index.js
+++ HomePage/js/index.js
@@ -1,55 +1,70 @@
-window.onload= function(){
+// typing as printer on webpage
+import Typed from './typed.js';
+
+function typedText() {
+	var typed = new Typed('#about .typedtext', {
+	strings: ["Freelancer", "Web developer."],
+	typeSpeed: 100,
+	loop: true,
+	showCursor: false,
+});
+}
+
+
+
+
+window.onload = function() {
 	// nav bar js part
 	let navBar = document.querySelector(".nav-bar");
 	let faBars = document.querySelector(".fa-bars-cursor");
 	let navLastSpanI = document.querySelector('.nav span:last-child i');
 	// let navLastSpan = document.querySelector('.nav span:last-child');
-	faBars.addEventListener('mouseenter', function(){
+	faBars.addEventListener('mouseenter', function() {
 		faBars.style.cursor = "pointer";
 	})
 	var flag = true;
-	faBars.addEventListener('click',  function(){
-		if(flag == true){
+	faBars.addEventListener('click', function() {
+		if (flag == true) {
 			navBar.style.display = "flex";
 			navBar.style.alignItems = "center";
 			// navLastSpan.innerHTML= '<i class = "fa fa-times fa-bars-cursor"></i>';
 			flag = false;
 			navLastSpanI.setAttribute('class', "fa fa-times fa-bars-cursor");
-			
-		}else {
+
+		} else {
 			navBar.style.display = "none";
 			// navLastSpan.innerHTML= '<i class = "fa fa-bars fa-bars-cursor"></i>';
 			navLastSpanI.setAttribute('class', "fa  fa-bars fa-bars-cursor");
 			flag = true;
-		}	
+		}
 	});
 	// swiper button js for display
 	let swiperButtonL = document.querySelector(".swiper-button-l");
 	let swiperButtonR = document.querySelector(".swiper-button-r");
 	let slideShow = document.querySelector(".slideshow");
-	
-	slideShow.addEventListener('mouseenter', function(){
+
+	slideShow.addEventListener('mouseenter', function() {
 		swiperButtonL.style.display = "block";
 		swiperButtonR.style.display = "block";
-		
+
 	});
-	slideShow.addEventListener('mouseleave', function(){
+	slideShow.addEventListener('mouseleave', function() {
 		swiperButtonL.style.display = "none";
 		swiperButtonR.style.display = "none";
-		
+
 	});
 	// skill progress bar
 	var solidSkillHtml = document.querySelector('.solid-skill-html');
-	var solidSkillJs =  document.querySelector('#skill .solid-skill-js');
+	var solidSkillJs = document.querySelector('#skill .solid-skill-js');
 	var solidSkillJquery = document.querySelector("#skill .solid-skill-jquery");
 	var solidSkillNode = document.querySelector("#skill .solid-skill-node");
 	var solidSkillWp = document.querySelector("#skill .solid-skill-wp");
 	var solidSkillJava = document.querySelector("#skill .solid-skill-java-shell");
-	
+
 	var skills = document.querySelector('#skill');
 	var skillTop = skills.offsetTop;
-	
-	
+
+
 	//obtain DOM for experience animation
 	// var experiences = document.querySelector('#experiences');
 	var expert = document.querySelector("#experiences .expert");
@@ -62,27 +77,25 @@ window.onload= function(){
 	var eduTop = education.offsetTop;
 	var qualicerliBoxs = document.querySelectorAll('.quanlicerli-box');
 	// document scroll event
-	document.addEventListener("scroll", function(){
-		if(window.pageYOffset >= 0.5*skill.offsetTop)
-		{
+	document.addEventListener("scroll", function() {
+		if (window.pageYOffset >= 0.5 * skill.offsetTop) {
 			solidSkillHtml.style.width = "90%";
 			solidSkillJs.style.width = "85%";
 			solidSkillJquery.style.width = "85%";
-			solidSkillNode.style.width = "80%"; 
+			solidSkillNode.style.width = "80%";
 			solidSkillWp.style.width = "80%";
 			solidSkillJava.style.width = "80%";
 		}
 		// experience animatin
-		if(window.pageYOffset >= experiTop)
-		{
+		if (window.pageYOffset >= experiTop) {
 			expertBox1.style.opacity = "1.0";
 			expertBox2.style.opacity = "1.0";
 			expertBox3.style.opacity = "1.0";
 		}
 		// education animation
-		if(window.pageYOffset >= eduTop) {
-			for( let i = 0; i < qualicerliBoxs.length; i++){
-				for( let j = 0; j < qualicerliBoxs[i].children.length; j++){
+		if (window.pageYOffset >= eduTop) {
+			for (let i = 0; i < qualicerliBoxs.length; i++) {
+				for (let j = 0; j < qualicerliBoxs[i].children.length; j++) {
 					qualicerliBoxs[i].children[j].style.opacity = "1.0";
 					qualicerliBoxs[i].children[j].style.marginLeft = "0rem";
 					qualicerliBoxs[i].children[j].style.marginRight = "0rem";
@@ -90,7 +103,8 @@ window.onload= function(){
 			}
 		}
 	});
-		
-		
 	
+	// To type the text in About of freelancer and web developer
+	typedText();
+
 }
---
+++ JavaScript/TypeJs/typeJsTest.js
@@ -0,0 +1,12 @@
+// import Typed from './js/typed.min.js';
+import Typed from './js/typed.js';
+// window.addEventListener('load', function(){
+var typed = new Typed('.app', {
+			  strings: ["First sentence.", "Second sentence."],
+			  typeSpeed: 30,
+			  loop: true,
+			  showCursor: false,
+			});	
+// })
+	
+	
