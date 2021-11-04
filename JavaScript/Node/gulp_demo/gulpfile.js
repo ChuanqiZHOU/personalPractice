4b5cf8b22a77dd135d7bc7e5a22ab3b7bde810cb
Bill ZHOU <chuanqizhou@gmail.com>
2021-11-04 15:21:49 +1300

    gulp htmlmin fileinclude csso

--- JavaScript/Node/gulp_demo/gulpfile.js
+++ JavaScript/Node/gulp_demo/gulpfile.js
@@ -1,4 +1,7 @@
 const gulp = require('gulp');
+const htmlmin = require('gulp-htmlmin');
+const fileinclude = require('gulp-file-include');
+const gulpcsso = require('gulp-csso');
 
 gulp.task('first', async() => {
 
@@ -6,3 +9,16 @@ gulp.task('first', async() => {
         .pipe(gulp.dest('./dist'));
 
 });
+//html code compressing
+gulp.task('htmlmin', async() => {
+    await gulp.src('./src/*.html')
+        .pipe(fileinclude()) // extract common code
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('./dist'));
+});
+// css code compressing
+gulp.task('csso', async() => {
+    await gulp.src('./src/css/*.css')
+        .pipe(gulpcsso())
+        .pipe(gulp.dest('./dist/css'));
+})
