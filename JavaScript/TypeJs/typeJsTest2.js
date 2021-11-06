46b8799c65034f39ab8cb8f952d1c47dc316c2b6
Bill ZHOU <chuanqizhou@gmail.com>
2021-11-06 13:04:22 +1300

    update quote from internet source

---
+++ JavaScript/TypeJs/typeJsTest2.js
@@ -0,0 +1,11 @@
+typedText();
+
+
+function typedText() {
+    var typed = new Typed('.app', {
+        strings: ["Freelancer", "Web developer."],
+        typeSpeed: 100,
+        loop: true,
+        showCursor: false,
+    });
+}
