4e3da512fb6a71069db8e228e3a81d035e3b3ead
Bill ZHOU <chuanqizhou@gmail.com>
2021-11-12 16:53:37 +1300

    express example

---
+++ JavaScript/Node/blog/app.js
@@ -0,0 +1,33 @@
+//import Express
+const express = require('express');
+
+const path = require('path');
+
+const app = express();
+
+//the position of template
+app.set('views', path.join(__dirname, 'views'));
+
+// the file style of template
+
+app.set('view engine', 'art');
+
+// which template is used?
+app.engine('art', require('express-art-template'));
+
+const home = require('./route/home');
+
+const admin = require('./route/admin');
+
+
+// open the static sources
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/home', home);
+
+app.use('/admin', admin);
+
+
+
+app.listen(80);
+console.log('server is running')
