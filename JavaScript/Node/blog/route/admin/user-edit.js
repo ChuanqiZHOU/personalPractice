1f2ffaef563b3c3b76908ccee5c5a92de5d5b2ec
ChuanqiZHOU <chuanqizhou@gmail.com>
2021-11-21 21:18:04 +1300

    pega list and previous and next buton

--- JavaScript/Node/blog/route/admin/user-edit.js
+++ JavaScript/Node/blog/route/admin/user-edit.js
@@ -1,6 +1,23 @@
-module.exports = (req, res) => {
-    const { message } = req.query;
-    res.render('admin/user-edit', {
-        message: message
-    })
+const { User } = require('../../model/user');
+module.exports = async(req, res) => {
+    // obtain id para
+
+    const { message, id } = req.query;
+    // if the id is transfer
+    if (id) {
+        // modify action
+        let user = await User.findOne({ _id: id });
+        // invoke user edit page
+        res.render('admin/user-edit', {
+            message: message,
+            user: user
+        })
+    } else {
+        //add action
+        res.render('admin/user-edit', {
+            message: message
+        })
+    }
+
+
 }
--- JavaScript/Node/blog/route/admin/userPage.js
+++ JavaScript/Node/blog/route/admin/userPage.js
@@ -1,10 +1,28 @@
 const { User } = require('../../model/user')
 
 module.exports = async(req, res) => {
-    let users = await User.find({});
+    //accept current page 
+
+    let page = req.query.page || 1; // default is 1
+
+    //res.send(page);
+    // user list per page
+    let pagesize = 2;
+
+    //total user in database
+    let countNumber = await User.countDocuments({});
+
+    //total page
+    let total = Math.ceil(countNumber / pagesize);
+    // page start
+
+    let start = (page - 1) * pagesize;
+    // find the total users 
+    let users = await User.find({}).limit(pagesize).skip(start);
     //res.send(users);
     res.render('admin/user', {
-
         users: users,
+        page: page,
+        total: total
     })
 }
