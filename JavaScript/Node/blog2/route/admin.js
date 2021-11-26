0b936e35544edc4e858a190ff3107f4ed3f24ec4
ChuanqiZHOU <chuanqizhou@gmail.com>
2021-11-26 21:01:45 +1300

    blog2 updating

--- JavaScript/Node/blog2/route/admin.js
+++ JavaScript/Node/blog2/route/admin.js
@@ -2,8 +2,27 @@ const express = require('express');
 
 const admin = express.Router();
 
-admin.get('/', (req, res) => {
-    res.send('welcome to admin page')
+admin.get('/login', (req, res) => {
+    res.render('admin/login.art')
+})
+
+// for login in login.art
+admin.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    if (email.trim().length == 0 || password.trim().length == 0) {
+        // return res.status(400).send('email or password is wrong')
+        return res.status(400).render('admin/error', {
+            msg: 'email or password is wrong'
+        })
+    }
 
 });
+
+admin.get('/user', (req, res) => {
+        res.render('admin/user.art')
+    })
+    // admin.get('/', (req, res) => {
+    //     res.send('welcome to admin page')
+
+// });
 module.exports = { admin }
