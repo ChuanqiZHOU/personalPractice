fa96e99b6ced743abfab1c5540256fbf641114bf
ChuanqiZHOU <chuanqizhou@gmail.com>
2021-11-27 20:51:19 +1300

    React

--- JavaScript/Node/blog2/route/admin.js
+++ JavaScript/Node/blog2/route/admin.js
@@ -2,12 +2,15 @@ const express = require('express');
 
 const admin = express.Router();
 
+const { User } = require('../model/user')
+
+
 admin.get('/login', (req, res) => {
     res.render('admin/login.art')
 })
 
 // for login in login.art
-admin.post('/login', (req, res) => {
+admin.post('/login', async(req, res) => {
     const { email, password } = req.body;
     if (email.trim().length == 0 || password.trim().length == 0) {
         // return res.status(400).send('email or password is wrong')
@@ -15,7 +18,18 @@ admin.post('/login', (req, res) => {
             msg: 'email or password is wrong'
         })
     }
-
+    // to check user information in database
+    let user = await User.findOne({ email });
+    if (user) {
+        // password comparing
+        if (password == user.password) {
+            res.send('login sucessful')
+        } else {
+            res.status(400).render('admin/error', { msg: 'email or password is error' })
+        }
+    } else {
+        res.status(400).render('admin/error', { msg: 'email or password is error' })
+    }
 });
 
 admin.get('/user', (req, res) => {
