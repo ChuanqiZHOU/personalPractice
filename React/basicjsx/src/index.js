4386c1aa8e86d71009f3b79cb58e1d09fce1d03c
ChuanqiZHOU <chuanqizhou@gmail.com>
2021-11-28 23:52:52 +1300

    react for project boader
    verify the input is null or not

--- React/basicjsx/src/index.js
+++ React/basicjsx/src/index.js
@@ -21,6 +21,11 @@ class App extends React.Component {
       { id: 2, name: "rose", content: "板凳~" },
       { id: 3, name: "tom", content: "楼主好人" },
     ],
+    // 评论人
+    userName: '',
+    //评论内容
+    userContent: ''
+
   };
 //渲染函数
   renderList() {
@@ -37,26 +42,67 @@ class App extends React.Component {
       </ul>
     ); 
   }
+// 表单处理函数
+  
+  handleForm = (e) => {
+    const { name, value } = e.target;
+    this.setState({
+       [name]: value
+        })  
+  }
+//发表评论：
+  addComment = () => {
+     
+    const { comments, userName, userContent } = this.state
+    //console.log(userName, userContent)
+    
+    // 非空校验
+    if(userName.trim() === '' || userContent.trim() === '') {
+      alert('输入内容')
+      return
+    }
+    
+    const newComments = [{
+      id: Math.random(),
+      name: userName,
+      content: userContent
+    }, ...comments
+    ]
 
-
+    this.setState({
+      comments: newComments,
+      userName: "", // 清除文本框
+      userContent: "" //清除文本框
+    });
+  }
   render() {
+    const { userName, userContent } = this.state;
     return (
       <div className="app">
         <div>
-          <input className="user" type="text" placeholder="请输入评论人" />
+          <input
+            className="user"
+            type="text"
+            placeholder="请输入评论人"
+            value={userName}
+            name="userName"
+            onChange={this.handleForm}
+          />
           <br />
           <textarea
             className="content"
             cols="30"
             rows="10"
             placeholder="请输入评论内容"
+            value={userContent}
+            onChange={this.handleForm}
+            name="userContent"
           />
           <br />
-          <button> 发表评论 </button>
+          <button onClick= {this.addComment}> 发表评论 </button>
         </div>
         {/* 条件渲染 */}
         {this.renderList()}
-        
       </div>
     );
   }
