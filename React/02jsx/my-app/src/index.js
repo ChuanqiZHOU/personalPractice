9b0f9e4c5e7fadecfb9ea3559346c45c2411de2f
ChuanqiZHOU <chuanqizhou@gmail.com>
2021-12-12 04:32:37 +1300

    React props

--- React/02jsx/my-app/src/index.js
+++ React/02jsx/my-app/src/index.js
@@ -209,21 +209,208 @@ import ReactDOM from 'react-dom';
 // }
 
 //非受控组件
-class App extends React.Component {
-    constructor() {
-        super()
-        this.txtRef= React.createRef()
-    }
-    getChange = e => {
-        console.log('文本框的值:', this.txtRef.current.value)
-    }
-    render() {
-        return (
-            <div>
-            <input type="text" ref={this.txtRef} />
-                <button onClick={this.getChange}>get input</button>
-            </div>
-        )
-    }
-}
-ReactDOM.render(<App/>, document.getElementById('root'))
+// class App extends React.Component {
+//     constructor() {
+//         super()
+//         this.txtRef= React.createRef()
+//     }
+//     getChange = e => {
+//         console.log('文本框的值:', this.txtRef.current.value)
+//     }
+//     render() {
+//         return (
+//             <div>
+//             <input type="text" ref={this.txtRef} />
+//                 <button onClick={this.getChange}>get input</button>
+//             </div>
+//         )
+//     }
+// }
+
+// props 的传递数据
+// function App(props) {
+//     console.log(props);
+//     return (
+//         <div>THIS IS NAME: {props.name}</div>
+//     )
+// }
+
+// class App extends React.Component {
+//     render() {
+//         return (
+//             <div>this is {this.props.name} { this.props.colors[1]}from class
+//             </div>
+//         )
+//     }
+// }
+
+// ReactDOM.render(
+//   <App name="jack" colors={["red", "green"]} />,
+//   document.getElementById("root")
+// );
+
+// props 父到子的数据传递
+// class Parent extends React.Component {
+//   state = {
+//     lastName: "王",
+//   };
+//   render() {
+//     return (
+//       <div>
+//         传递给子组件数据 <Child Xing={this.state.lastName} ></Child>
+//       </div>
+//     )
+//   }
+// }
+
+// class Child extends React.Component {
+//   render() {
+//     return <div>子组件接收的数据:{this.props.Xing}</div>;
+//   }
+// }
+
+// ReactDOM.render(<Parent/>, document.getElementById('root'))
+
+//props 子到父传递数据
+// class Parent extends React.Component {
+//     state = {
+//         parentMsg:''
+//     }
+//     getChildMsg = (data) => {
+//         console.log('来自child的数据', data)
+//         this.setState({
+//             parentMsg: data
+//         })
+// }
+//   render() {
+//     return (
+//       <div>
+//             父组件中：{this.state.parentMsg}
+//             <Child getMsg={this.getChildMsg}></Child>
+//       </div>
+//     );
+//   }
+// }
+
+// class Child extends React.Component {
+//     state = {
+//         child: 'red'
+//     }
+//     handleChild = () => {
+//     this.props.getMsg(this.state.child)
+// }
+//   render() {
+//       return (
+//           <div>
+//               <div>这是要传出的数据{this.state.child}</div>
+//            <button onClick= {this.handleChild}>点击传递子组件数据</button>
+//           </div>
+          
+//       )
+      
+
+//   }
+// }
+
+// 兄弟间通讯
+
+// class Parent extends React.Component{
+//     state = {
+//         count:0
+//     }
+
+//     getMsgFrom2 = () => {
+//         this.setState({
+//             count: this.state.count+1
+//         })
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <div><Child1 count={this.state.count}/></div>
+//                 <div><Child2 getMsg={this.getMsgFrom2}/></div>
+//             </div>
+//     )
+// }
+// }
+// class Child1 extends React.Component{
+//     render() {
+//         return (
+//             <div>计数器:{this.props.count}</div>
+//     )
+// }
+// }
+
+// class Child2 extends React.Component {
+
+
+//     handlePlus= ()=>{
+        
+//         this.props.getMsg()
+//     }
+//     render() {
+//         return (
+//             <button onClick={this.handlePlus}>+1</button>
+//         )
+
+//     }
+// }
+// ReactDOM.render(<Parent />,document.getElementById("root"))
+
+// context 的使用
+// const { Provider, Consumer } = React.createContext();
+// class App extends React.Component{
+//     render() {
+//         return (
+//             <Provider value="pink">
+//             <div>
+//                 <Node ></Node>
+//                 </div></Provider>
+            
+//         )
+//     }
+// }
+
+// class Node extends React.Component {
+//   render() {
+//       return (
+        
+//       <div>
+//         <Child></Child>
+//       </div>
+//     );
+//   }
+// }
+
+// class Child extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//             I AM CHILD
+//             <Consumer>
+//                 {
+//                     data => <span>子节点的参数{ data}</span>
+//                 }
+//             </Consumer>
+//       </div>
+//     );
+//   }
+// }
+
+// ReactDOM.render(<App />, document.getElementById("root")
+// 
+// children 属性
+const APP = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h1>组件标签的子节点：{props.children}</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(
+  <APP>i am children node</APP>,
+  document.getElementById("root")
+);
